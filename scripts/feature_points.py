23e2922bb77f4e636e7ba27aa67fa893eb27ea80
RileyFranklin <rfrankl@purdue.edu>
2023-04-02 15:31:35 -0400

    Realtime feature matching

--- scripts/feature_points.py
+++ scripts/feature_points.py
@@ -5,7 +5,7 @@ from rclpy.node import Node
 from cv_bridge import CvBridge
 
 import cv2
-
+import numpy as np
 from sensor_msgs.msg import Image
 
 
@@ -24,14 +24,17 @@ class FeaturePoints(Node):
         FLANN_INDEX_KDTREE = 1
         index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
         search_params = dict(checks=50)
-        
+        self.first_img_=0
         self.flann_ = cv2.FlannBasedMatcher(index_params,search_params)
+        self.kp_prev_=None
+        self.img_prev_=None
+        self.des_prev_=None
         
     def listener_callback(self, msg):
         img = self.br_.imgmsg_to_cv2(msg)
 
         method = 'orb'
-    
+
         if method == 'surf':
             surf = cv2.SURF_create()
             kp, des = surf.detectAndCompute(img, None)
@@ -41,12 +44,32 @@ class FeaturePoints(Node):
             kp, des = sift.detectAndCompute(img, None)
             img2 = cv2.drawKeypoints(img, kp, None, (255,0,0), 4)
         elif method == 'orb':
-            orb = cv2.ORB_create(nfeatures=50)
+            orb = cv2.ORB_create(nfeatures=200)
             kp, des = orb.detectAndCompute(img, None)
-            kp_prev = kp
-            des_prev = des
-            #matches = self.flann_.knnMatch(des_c,des_prev,k=2) 
-            img2 = cv2.drawKeypoints(img, kp, None, (255,0,0), 4)
+            if self.img_prev_ is None:
+                img2=img
+                self.kp_prev_ = kp
+                self.des_prev_ = des
+                self.img_prev_ = img
+            else:
+                matches = self.flann_.knnMatch(np.float32(des),np.float32(self.des_prev_),k=2)
+                # Need to draw only good matches, so create a mask
+                matchesMask = [[0,0] for i in range(len(matches))]
+                # ratio test as per Lowe's paper
+                for i,(m,n) in enumerate(matches):
+                    if m.distance < 0.60*n.distance:
+                        matchesMask[i]=[1,0]
+                draw_params = dict(matchColor = (0,255,0),
+                                singlePointColor = (255,0,0),
+                                matchesMask = matchesMask,
+                                flags = cv2.DrawMatchesFlags_DEFAULT)
+                img2 = cv2.drawMatchesKnn(img,kp,self.img_prev_,self.kp_prev_,matches,None,**draw_params)
+                self.kp_prev_ = kp
+                self.des_prev_ = des
+                self.img_prev_ = img  
+            
+            
+            
         elif method == 'none':
             img2 = img
         else:
