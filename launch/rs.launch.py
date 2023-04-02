e0400c15d01b2d4c7fb41faf956dd86f5580540c
RileyFranklin <rfrankl@purdue.edu>
2023-04-02 12:15:41 -0400

    nxp purt to repo

---
+++ launch/rs.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    return LaunchDescription([
+        ExecuteProcess(cmd=['ros2', 'bag', 'record',
+            '/camera/color/image_rect_raw',
+            '/camera/aligned_depth_to_color/image_raw'
+        ]),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([get_package_share_directory('realsense2_camera') ,'/launch/rs_launch.py']),
+            launch_arguments={
+            	#'pointcloud.enable': 'true',
+                #'filters': 'pointcloud',
+                'align_depth.enable': 'true',
+                'depth_fps': '15.0',
+                'depth_width': '640',
+                'depth_height': '480',
+                'color_fps': '15.0',
+                'color_width': '640',
+                'color_height': '480',
+		        #'pointcloud.ordered_pc': 'true',
+                #'camera_info_url': 'package://cuesr/config/depth_5_19_22.yaml',
+                }.items(),
+        ),
+        Node(package='nxp_purt', executable='feature_points.py', output='screen')
+    ])
---
+++ scripts/feature_points.py
@@ -0,0 +1,70 @@
+#!/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from cv_bridge import CvBridge
+
+import cv2
+
+from sensor_msgs.msg import Image
+
+
+class FeaturePoints(Node):
+
+    def __init__(self):
+        super().__init__('feature_points')
+        self.subscription_ = self.create_subscription(
+            Image,
+            '/camera/color/image_raw',
+            self.listener_callback,
+            10)
+        self.publisher_ = self.create_publisher(Image, 'feature_points', 10)
+        self.br_ = CvBridge()
+        
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks=50)
+        
+        flann_ = cv.FlannBasedMatcher(index_params,search_params)
+        
+    def listener_callback(self, msg):
+        img = self.br_.imgmsg_to_cv2(msg)
+
+        method = 'orb'
+    
+        if method == 'surf':
+            surf = cv2.SURF_create()
+            kp, des = surf.detectAndCompute(img, None)
+            img2 = cv2.drawKeypoints(img, kp, None, (255,0,0), 4)
+        elif method == 'sift':
+            sift = cv2.SIFT_create()
+            kp, des = sift.detectAndCompute(img, None)
+            img2 = cv2.drawKeypoints(img, kp, None, (255,0,0), 4)
+        elif method == 'orb':
+            orb = cv2.ORB_create(nfeatures=50)
+            kp_c, des_c = orb.detectAndCompute(img, None)
+            
+            kp_prev = kp_c
+            des_prev = des_c
+            matches = flann.knnMatch(des_c,des_prev,k=2) 
+            #img2 = cv2.drawKeypoints(img, kp, None, (255,0,0), 4)
+        elif method == 'none':
+            img2 = img
+        else:
+            raise ValueError('unknown method')
+       
+        out_msg = self.br_.cv2_to_imgmsg(img2, encoding='rgb8')
+        self.publisher_.publish(out_msg)
+        return
+
+
+def main(args=None):
+    print("opencv version", cv2.__version__, cv2.__file__)
+    rclpy.init(args=args)
+    feature_points = FeaturePoints()
+    rclpy.spin(feature_points)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
