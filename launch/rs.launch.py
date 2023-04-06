38ad99b6091dc41ed0120b30b8fca98d53ed0ffd
RileyFranklin <rfrankl@purdue.edu>
2023-04-06 11:10:44 -0400

    Live pull matches from camera topics

--- launch/rs.launch.py
+++ launch/rs.launch.py
@@ -7,19 +7,20 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     return LaunchDescription([
-        ExecuteProcess(cmd=['ros2', 'bag', 'record',
-            '/camera/color/image_raw',
-            '/camera/aligned_depth_to_color/image_raw'
-        ]),
+        #ExecuteProcess(cmd=['ros2', 'bag', 'record',
+        #    '/camera/color/image_raw',
+        #    '/camera/color/camera_info'
+        #    '/camera/depth/color/points'
+        #]),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([get_package_share_directory('realsense2_camera') ,'/launch/rs_launch.py']),
             launch_arguments={
-                #'pointcloud.enable': 'true',
+                'pointcloud.enable': 'true',
                 'align_depth.enable': 'true',
-                'depth_module.profile': '1280,720,30',
-                'rgb_camera.profile': '1280,720,30',
-		        #'ordered_pc': 'true',
+                'depth_module.profile': '848,480,60',
+                'rgb_camera.profile': '848,480,60',
+        		'pointcloud.ordered_pc': 'true',
                 }.items(),
         ),
-        Node(package='casadi_vio', executable='feature_points.py', output='screen')
+        #Node(package='casadi_vio', executable='feature_points.py', output='screen')
     ])
--- scripts/feature_points.py
+++ scripts/feature_points.py
@@ -6,7 +6,9 @@ from cv_bridge import CvBridge
 
 import cv2
 import numpy as np
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, PointCloud2
+from std_msgs.msg import Int32
+import struct
 
 
 class FeaturePoints(Node):
@@ -19,16 +21,29 @@ class FeaturePoints(Node):
             self.listener_callback,
             10)
         self.publisher_ = self.create_publisher(Image, 'feature_points', 10)
+        self.subscription_ = self.create_subscription(
+            PointCloud2,
+            '/camera/depth/color/points',
+            self.listener_callback_pc,
+            10)
+        self.publish = self.create_publisher(Int32, 'width', 10)
         self.br_ = CvBridge()
         
-        FLANN_INDEX_KDTREE = 1
-        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        FLANN_INDEX_LSH = 5
+        index_params= dict(algorithm = FLANN_INDEX_LSH,
+                   table_number = 6, # 12
+                   #key_size = 12,     # 20
+                   #multi_probe_level = 1,
+                   trees=5) #2
         search_params = dict(checks=50)
         self.first_img_=0
         self.flann_ = cv2.FlannBasedMatcher(index_params,search_params)
         self.kp_prev_=None
         self.img_prev_=None
         self.des_prev_=None
+        self.point_cloud_=None
+        self.u_=10
+        self.v_=12
         
     def listener_callback(self, msg):
         img = self.br_.imgmsg_to_cv2(msg)
@@ -66,8 +81,12 @@ class FeaturePoints(Node):
                 img2 = cv2.drawMatchesKnn(img,kp,self.img_prev_,self.kp_prev_,matches,None,**draw_params)
                 self.kp_prev_ = kp
                 self.des_prev_ = des
-                self.img_prev_ = img  
-            
+                self.img_prev_ = img 
+                img_idx=matches[0][0].queryIdx
+                
+                (self.u_,self.v_)=kp[img_idx].pt
+                self.u_=int(round(self.u_))
+                self.v_=int(round(self.v_))
             
             
         elif method == 'none':
@@ -79,6 +98,38 @@ class FeaturePoints(Node):
         self.publisher_.publish(out_msg)
         return
 
+    def listener_callback_pc(self, msg):
+        #maybe try to chop off rgb values
+        ## get width and height of 2D point cloud data
+        pCloud=msg
+        width = 400 #pCloud.width
+        height = 200#pCloud.height
+        
+        # Convert from u (column / width), v (row/height) to position in array
+        # where X,Y,Z data starts
+        arrayPosition = self.v_*pCloud.row_step + self.u_*pCloud.point_step
+        
+        #compute position in array where x,y,z data start
+        PosX = arrayPosition + pCloud.fields[0].offset; #// X has an offset of 0
+        PosY = arrayPosition + pCloud.fields[1].offset; #// Y has an offset of 4
+        PosZ = arrayPosition + pCloud.fields[2].offset; #// Z has an offset of 8
+        
+        #X = struct.unpack('f',bytearray(pCloud.data[arrayPosX:arrayPosX+3]))
+        Xbyte=bytearray([pCloud.data[PosX],pCloud.data[PosX+1],pCloud.data[PosX+2],pCloud.data[PosX+3]])
+        Ybyte=bytearray([pCloud.data[PosY],pCloud.data[PosY+1],pCloud.data[PosY+2],pCloud.data[PosY+3]])
+        Zbyte=bytearray([pCloud.data[PosZ],pCloud.data[PosZ+1],pCloud.data[PosZ+2],pCloud.data[PosZ+3]])
+        X = struct.unpack('f',Xbyte)
+        Y = struct.unpack('f',Ybyte)
+        Z = struct.unpack('f',Zbyte)
+        
+        print(X,',',Y,',',Z)
+
+
+
+
+
+        
+        return
 
 def main(args=None):
     print("opencv version", cv2.__version__, cv2.__file__)
