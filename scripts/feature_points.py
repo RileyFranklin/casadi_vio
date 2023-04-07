16cf7bf7dfa4dcdcfa05c37ffface8b5236d1180
Scoops12 <coope263@purdue.edu>
2023-04-07 12:22:57 -0400

    Added pointcloud extraction with feature points

--- scripts/feature_points.py
+++ scripts/feature_points.py
@@ -1,5 +1,8 @@
 #!/bin/env python3
 
+import array
+from typing import Iterable, List, NamedTuple, Optional
+
 import rclpy
 from rclpy.node import Node
 from cv_bridge import CvBridge
@@ -9,6 +12,7 @@ import numpy as np
 from sensor_msgs.msg import Image, PointCloud2
 from std_msgs.msg import Int32
 import struct
+from sensor_msgs_py import point_cloud2
 
 
 class FeaturePoints(Node):
@@ -42,6 +46,9 @@ class FeaturePoints(Node):
         self.img_prev_=None
         self.des_prev_=None
         self.point_cloud_=None
+        self.pc_prev = None
+        self.pc = None
+        self.nfeatures = 200
         self.u_=10
         self.v_=12
         
@@ -59,14 +66,15 @@ class FeaturePoints(Node):
             kp, des = sift.detectAndCompute(img, None)
             img2 = cv2.drawKeypoints(img, kp, None, (255,0,0), 4)
         elif method == 'orb':
-            orb = cv2.ORB_create(nfeatures=200)
+            orb = cv2.ORB_create(nfeatures=self.nfeatures)
             kp, des = orb.detectAndCompute(img, None)
             if self.img_prev_ is None:
                 img2=img
                 self.kp_prev_ = kp
                 self.des_prev_ = des
                 self.img_prev_ = img
-            else:
+            # Catches if zero features are calculated.
+            elif (len(kp) > 0) and (len(self.kp_prev_) > 0):
                 matches = self.flann_.knnMatch(np.float32(des),np.float32(self.des_prev_),k=2)
                 # Need to draw only good matches, so create a mask
                 matchesMask = [[0,0] for i in range(len(matches))]
@@ -79,57 +87,148 @@ class FeaturePoints(Node):
                                 matchesMask = matchesMask,
                                 flags = cv2.DrawMatchesFlags_DEFAULT)
                 img2 = cv2.drawMatchesKnn(img,kp,self.img_prev_,self.kp_prev_,matches,None,**draw_params)
+
+                # Publish img2 to msg
+                out_msg = self.br_.cv2_to_imgmsg(img2, encoding='rgb8')
+                self.publisher_.publish(out_msg)
+
+                # Get only the good matches
+                good_m2 = []
+                for i, match in enumerate(matchesMask):
+                    if match[0] == 1:
+                        good_m2.append(matches[i])
+
+                # Loop through all good points and store their x and y pixel location
+                list_pxl_prev = []
+                list_pxl = []
+
+                # Skips xyz point collection if not enough features are detected.
+                if not (len(kp) < self.nfeatures) and not (len(self.kp_prev_) < self.nfeatures):
+
+                    # For each match...
+                    for mat in good_m2:
+
+                        # Get the matching keypoints for each of the images
+                        img0_idx = mat[0].queryIdx
+                        img1_idx = mat[0].trainIdx
+
+                        # x - columns
+                        # y - rows
+                        # Get the coordinates
+                        (x0, y0) = self.kp_prev_[img0_idx].pt
+                        (x1, y1) = kp[img1_idx].pt
+
+                        # Append to each list
+                        list_pxl_prev.append((int(round(x0)), int(round(y0))))
+                        list_pxl.append((int(round(x1)), int(round(y1))))
+
+                    # Use get_points_efficient to get xyz points for all good matches
+                    if self.pc_prev is not None:
+                        xyz_points_prev = self.read_points_efficient(self.pc_prev, uvs=list_pxl_prev, field_names = ("x", "y", "z"))
+                        xyz_points = self.read_points_efficient(self.pc, uvs=list_pxl, field_names = ("x", "y", "z"))
+
+                        # print(xyz_points)
+                else:
+                    print("Not enough features detected. Skipping frame...")
+
+                # # Testing
+                # img_idx=matches[0][0].queryIdx
+                
+                # (self.u_,self.v_)=kp[img_idx].pt
+                # self.u_=int(round(self.u_))
+                # self.v_=int(round(self.v_))
+
+                # Update previous values in self
                 self.kp_prev_ = kp
                 self.des_prev_ = des
                 self.img_prev_ = img 
-                img_idx=matches[0][0].queryIdx
-                
-                (self.u_,self.v_)=kp[img_idx].pt
-                self.u_=int(round(self.u_))
-                self.v_=int(round(self.v_))
-            
+
+            else:
+                print("Not enough features detected. Skipping frame...")
             
         elif method == 'none':
             img2 = img
         else:
             raise ValueError('unknown method')
        
-        out_msg = self.br_.cv2_to_imgmsg(img2, encoding='rgb8')
-        self.publisher_.publish(out_msg)
         return
 
     def listener_callback_pc(self, msg):
-        #maybe try to chop off rgb values
-        ## get width and height of 2D point cloud data
-        pCloud=msg
-        width = 400 #pCloud.width
-        height = 200#pCloud.height
-        
-        # Convert from u (column / width), v (row/height) to position in array
-        # where X,Y,Z data starts
-        arrayPosition = self.v_*pCloud.row_step + self.u_*pCloud.point_step
-        
-        #compute position in array where x,y,z data start
-        PosX = arrayPosition + pCloud.fields[0].offset; #// X has an offset of 0
-        PosY = arrayPosition + pCloud.fields[1].offset; #// Y has an offset of 4
-        PosZ = arrayPosition + pCloud.fields[2].offset; #// Z has an offset of 8
-        
-        #X = struct.unpack('f',bytearray(pCloud.data[arrayPosX:arrayPosX+3]))
-        Xbyte=bytearray([pCloud.data[PosX],pCloud.data[PosX+1],pCloud.data[PosX+2],pCloud.data[PosX+3]])
-        Ybyte=bytearray([pCloud.data[PosY],pCloud.data[PosY+1],pCloud.data[PosY+2],pCloud.data[PosY+3]])
-        Zbyte=bytearray([pCloud.data[PosZ],pCloud.data[PosZ+1],pCloud.data[PosZ+2],pCloud.data[PosZ+3]])
-        X = struct.unpack('f',Xbyte)
-        Y = struct.unpack('f',Ybyte)
-        Z = struct.unpack('f',Zbyte)
-        
-        print(X,',',Y,',',Z)
+        # Store current and previous PointCloud2 msg to self
+        if self.pc_prev is None:
+            self.pc = msg
+            self.pc_prev = msg
+            return
+        else:
+            self.pc_prev = self.pc
+            self.pc = msg
 
-
-
-
-
+        # # Riley Solution
+        # #maybe try to chop off rgb values
+        # ## get width and height of 2D point cloud data
+        # pCloud=msg
+        # width = 400 #pCloud.width
+        # height = 200#pCloud.height
         
+        # # Convert from u (column / width), v (row/height) to position in array
+        # # where X,Y,Z data starts
+        # arrayPosition = self.v_*pCloud.row_step + self.u_*pCloud.point_step
+        
+        # #compute position in array where x,y,z data start
+        # PosX = arrayPosition + pCloud.fields[0].offset; #// X has an offset of 0
+        # PosY = arrayPosition + pCloud.fields[1].offset; #// Y has an offset of 4
+        # PosZ = arrayPosition + pCloud.fields[2].offset; #// Z has an offset of 8
+        
+        # #X = struct.unpack('f',bytearray(pCloud.data[arrayPosX:arrayPosX+3]))
+        # Xbyte=bytearray([pCloud.data[PosX],pCloud.data[PosX+1],pCloud.data[PosX+2],pCloud.data[PosX+3]])
+        # Ybyte=bytearray([pCloud.data[PosY],pCloud.data[PosY+1],pCloud.data[PosY+2],pCloud.data[PosY+3]])
+        # Zbyte=bytearray([pCloud.data[PosZ],pCloud.data[PosZ+1],pCloud.data[PosZ+2],pCloud.data[PosZ+3]])
+        # X = struct.unpack('f',Xbyte)
+        # Y = struct.unpack('f',Ybyte)
+        # Z = struct.unpack('f',Zbyte)
+        
+        # print(X,',',Y,',',Z)
+
         return
+    
+    def read_points_efficient(
+        self,
+        cloud: PointCloud2,
+        field_names: Optional[List[str]] = None,
+        uvs: Optional[Iterable] = None) -> np.ndarray:
+
+        assert isinstance(cloud, PointCloud2), \
+            'Cloud is not a PointCloud2'        
+
+        # Grab only the data you want
+        if uvs is not None:
+            select_data = array.array('B', [])
+
+            # Loop through all provided pixel locations
+            for u,v in uvs:
+                start_ind = u*cloud.point_step + v*cloud.row_step
+                curr_data = cloud.data[start_ind:start_ind+cloud.point_step]
+
+                select_data += curr_data
+            points_len = len(uvs)
+        else:
+            select_data = cloud.data
+            points_len = cloud.width * cloud.height
+
+        # Cast bytes to numpy array
+        points = np.ndarray(
+            shape=(points_len, ),
+            dtype=point_cloud2.dtype_from_fields(cloud.fields, point_step=cloud.point_step),
+            buffer=select_data)
+        
+        # Keep only the requested fields
+        if field_names is not None:
+            assert all(field_name in points.dtype.names for field_name in field_names), \
+                'Requests field is not in the fields of the PointCloud!'
+            # Mask fields
+            points = points[list(field_names)]
+
+        return points
 
 def main(args=None):
     print("opencv version", cv2.__version__, cv2.__file__)
